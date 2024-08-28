3a44ffa593f8177c9cdd87d2d815b784dc4be1d4
Kadu-Brum-Gaviao <gaviaokadu73@gmail.com>
2024-08-27 22:03:43 -0300

    Add files via upload

---
+++ Semana 3/estruturaCondicional(Ordem Crescente).py
@@ -0,0 +1,8 @@
+numero1 = float(input('Digite o primeiro número: '))
+
+numero2 = float(input('Digite o segundo número: '))
+
+if numero1 > numero2:
+    print('A ordem dos números é:', numero1, ",", numero2)
+else:
+    print('A ordem dos números é:', numero2, numero1)
