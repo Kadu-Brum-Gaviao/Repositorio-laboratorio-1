c1ff4d3ace4f6129c12ed70d6c2f7dc5e8f4d810
Kadu-Brum-Gaviao <gaviaokadu73@gmail.com>
2024-10-01 22:13:18 -0300

    Add files via upload

---
+++ Exercício 8(Laço de Repetição).py
@@ -0,0 +1,10 @@
+"""
+Em um prédio com 10 moradores há três elevadores denominados A, B e C.
+Para otimizar o sistema de controle dos elevadores, desenvolva um
+programa em que cada morador informa qual o elevador que utiliza
+com mais frequência (A, B ou C).
+O algoritmo deve contabilizar o total de pessoas que usa
+cada um dos elevadores e mostrar estes totais e suas respectivas
+porcentagens no final.
+
+"""
